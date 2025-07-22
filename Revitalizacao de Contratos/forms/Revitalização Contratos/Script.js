5dec8dc43d6211a48f5676fb1fd933237dbd65f8
Gabriel Persike <50594940+Gabriel-Persike@users.noreply.github.com>
2025-07-22 13:28:19 -0300

    fetat/ Incluí as Bibliotecas DocxTemplater e CK5Editor, Cria as Funções para Buscar e preencher o Modelo do Contrato

--- Revitalizacao de Contratos/forms/Revitalização Contratos/Script.js
+++ Revitalizacao de Contratos/forms/Revitalização Contratos/Script.js
@@ -1,52 +1,62 @@
-$(document).ready(function(){          
-	
-//Chamada de Funções
-        preencherObrasDoUsuario()
-        BuscaFornecedores()
-        buscaBancos()
-        inicializarCalendario()
-        inicializarPeriodoLocacao();
-        
-//Mostrar/Ocultar Funções
-        $("#tipoContrato").on("change", function () {
-                if ($(this).val() == "Locação de Imóvel") {
-                        $("#formContainer").show();
-                }else{
-                        $("#formContainer").hide();
-                }
-        });
-        $("#caucao").on("change", function () {
-                if ($(this).val() == "Sim") {
-                        $("#divValorCaucao, #divDataPagamentoCaucao").show();
-                }else{
-                        $("#divValorCaucao, #divDataPagamentoCaucao").hide();
-                }
-        });
-        $("#tipoPagamento").on("change", function () {
-                if ($(this).val() == "Depósito") {
-                        $("#divPagamento").show();
-                }else{
-                        $("#divPagamento").hide();
-                }
-        });
-        $('#locador').on('change', function () {
-            var cgccfo = $(this).val();
-            if (cgccfo) {
-                buscarEnderecoFornecedor(cgccfo);
-            } else {
-                $(".endereco-fornecedor").slideUp();
-            }
-        });
-        
-//Máscaras
-        $('#agencia').mask('0000-0', {placeholder: "____-_"});
-        $('#contaCorrente').mask('00000-0', {placeholder: "_____-_"});              
-        $('#valorCaucao').maskMoney({
-            prefix: 'R$ ',
-            thousands: '.',
-            decimal: ',',
-            allowZero: true,
-            affixesStay: true  
-    });
-        
-})
+$(document).ready(function () {
+
+	//Chamada de Funções
+	bindings();
+	preencherObrasDoUsuario()
+	BuscaFornecedores()
+	buscaBancos()
+	inicializarCalendario()
+	inicializarPeriodoLocacao();
+
+	//Mostrar/Ocultar Funções
+	$("#tipoContrato").on("change", function () {
+		if ($(this).val() == "Locação de Imóvel") {
+			$("#formContainer").show();
+		} else {
+			$("#formContainer").hide();
+		}
+	});
+	$("#caucao").on("change", function () {
+		if ($(this).val() == "Sim") {
+			$("#divValorCaucao, #divDataPagamentoCaucao").show();
+		} else {
+			$("#divValorCaucao, #divDataPagamentoCaucao").hide();
+		}
+	});
+	$("#tipoPagamento").on("change", function () {
+		if ($(this).val() == "Depósito") {
+			$("#divPagamento").show();
+		} else {
+			$("#divPagamento").hide();
+		}
+	});
+	$('#locador').on('change', function () {
+		var cgccfo = $(this).val();
+		if (cgccfo) {
+			buscarEnderecoFornecedor(cgccfo);
+		} else {
+			$(".endereco-fornecedor").slideUp();
+		}
+	});
+
+	//Máscaras
+	$('#agencia').mask('0000-0', { placeholder: "____-_" });
+	$('#contaCorrente').mask('00000-0', { placeholder: "_____-_" });
+	$('#valorCaucao').maskMoney({
+		prefix: 'R$ ',
+		thousands: '.',
+		decimal: ',',
+		allowZero: true,
+		affixesStay: true
+	});
+
+});
+
+
+
+function bindings() {
+	// Amarra eventos e elementos do HTML, mantendo todas definições de evento agrupadas
+	$("#btnGerarArquivo").on("click", ()=>{buscaModeloDoContrato()});
+}
+
+
