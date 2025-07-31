e9dbed466ab3059e6b1edc7cafd478a63a4ebf3d
Gabriel Persike <50594940+Gabriel-Persike@users.noreply.github.com>
2025-07-31 18:23:20 -0300

    fix: Corrige identação do código

--- Revitalizacao de Contratos/forms/Revitalização Contratos/Script.js
+++ Revitalizacao de Contratos/forms/Revitalização Contratos/Script.js
@@ -1,59 +1,55 @@
-$(document).ready(function(){   
-        
-        preencherObrasDoUsuario()
-        buscaFornecedores()
-        buscaBancos()
-        inicializarCalendario()
-        inicializarPeriodoLocacao();
-        inicializaInputAnexo();
+$(document).ready(function () {
+    preencherObrasDoUsuario();
+    buscaFornecedores();
+    buscaBancos();
+    inicializarCalendario();
+    inicializarPeriodoLocacao();
+    inicializaInputAnexo();
 
-        $("#tipoContrato").on("change", function () {
-            if ($(this).val() === "Locação de Imóvel") {
-              $("#formContainer").show();
-              paginaAtual = 0;
-              mostrarPagina(paginaAtual);
-            } else {
-              $("#formContainer").hide();
-            }
-          });
+    $("#tipoContrato").on("change", function () {
+        if ($(this).val() === "Locação de Imóvel") {
+            $("#formContainer").show();
+            paginaAtual = 0;
+            mostrarPagina(paginaAtual);
+        } else {
+            $("#formContainer").hide();
+        }
+    });
 
-          $("#btn-avancar").on("click", avancarPagina);
-          $("#btn-voltar").on("click", voltarPagina);
-        
-        $("#caucao").on("change", function () {
-                if ($(this).val() == "Sim") {
-                        $("#divValorCaucao, #divDataPagamentoCaucao").show();
-                }else{
-                        $("#divValorCaucao, #divDataPagamentoCaucao").hide();
-                }
-        });
-        $("#tipoPagamento").on("change", function () {
-                if ($(this).val() == "Depósito") {
-                        $("#divPagamento, #divBanco").show();
-                }else{
-                        $("#divPagamento, #divBanco").hide();
-                }
-        });
-        $('#valorCaucao, #valorMensalAluguel').maskMoney({
-                prefix: 'R$ ',
-                thousands: '.',
-                decimal: ',',
-                allowZero: true,
-                affixesStay: true  
-            });
-        $('#agencia').mask('0000-0', {placeholder: "____-_"});
-        $('#contaCorrente').mask('00000-0', {placeholder: "_____-_"});
-                
-        $('#locador').on('change', function () {
-                var cgccfo = $(this).val();
+    $("#btn-avancar").on("click", avancarPagina);
+    $("#btn-voltar").on("click", voltarPagina);
 
-                if (cgccfo) {
-                        buscaInfosFornecedor(cgccfo);
-                } else {
-                    $(".endereco-fornecedor").slideUp();
-                }
-            });
+    $("#caucao").on("change", function () {
+        if ($(this).val() == "Sim") {
+            $("#divValorCaucao, #divDataPagamentoCaucao").show();
+        } else {
+            $("#divValorCaucao, #divDataPagamentoCaucao").hide();
+        }
+    });
+    $("#tipoPagamento").on("change", function () {
+        if ($(this).val() == "Depósito") {
+            $("#divPagamento, #divBanco").show();
+        } else {
+            $("#divPagamento, #divBanco").hide();
+        }
+    });
+    $("#valorCaucao, #valorMensalAluguel").maskMoney({
+        prefix: "R$ ",
+        thousands: ".",
+        decimal: ",",
+        allowZero: true,
+        affixesStay: true,
+    });
+    $("#agencia").mask("0000-0", { placeholder: "____-_" });
+    $("#contaCorrente").mask("00000-0", { placeholder: "_____-_" });
 
-})
+    $("#locador").on("change", function () {
+        var cgccfo = $(this).val();
 
-
+        if (cgccfo) {
+            buscaInfosFornecedor(cgccfo);
+        } else {
+            $(".endereco-fornecedor").slideUp();
+        }
+    });
+});
