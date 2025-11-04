74075c5c0c8a8392fa540de871915ecca31cae7a
Gabriel Persike <50594940+Gabriel-Persike@users.noreply.github.com>
2025-11-04 17:52:47 -0300

    Incluí função para verificar o status da assinatura e finalizar a solicitação quando o contrato for assinado

--- Revitalizacao de Contratos/workflow/scripts/Revitalização Contratos.intermediateconditional58.js
+++ Revitalizacao de Contratos/workflow/scripts/Revitalização Contratos.intermediateconditional58.js
@@ -1,3 +1,23 @@
 function intermediateconditional58() {
+    var numProcess = getValue("WKNumProces");
+    var ds = DatasetFactory.getDataset("ds_form_aux_wesign", null, [
+        DatasetFactory.createConstraint("numSolic",numProcess,numProcess,ConstraintType.MUST)
+    ], null);
 
+    log.info("verificaStatusAssinatura");
+    log.dir(ds);
+
+
+    for (var i = 0; i < ds.rowsCount; i++) {
+        var status = ds.getValue(i, "statusAssinatura");
+        log.info("status");
+        log.info(status);
+        hAPI.setCardValue("statusAssinatura", status)
+
+        if (status == "Pendente Assinatura") {
+            return false;
+        }
+    }
+
+    return true;
 }
