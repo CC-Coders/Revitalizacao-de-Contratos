7274da9cc91aba8f1b8ac1589d82202e851a1d2b
Gabriel Persike <50594940+Gabriel-Persike@users.noreply.github.com>
2025-12-04 13:02:15 -0300

    Corrige a tela da solicitação no modo VIEW

--- Revitalizacao de Contratos/forms/Revitalização Contratos/js/tipoContratoManager.js
+++ Revitalizacao de Contratos/forms/Revitalização Contratos/js/tipoContratoManager.js
@@ -3,16 +3,19 @@ function onChangeTipoContrato(that) {
     $("#paginationEquipamentos").hide();
     $("#paginationEquipamentos").addClass("hidden");
 
-    if ($(that).val() === "Locação de Imóvel") {
+    if ($(that).val() === "Locação de Imóvel" || $(that).text() === "Locação de Imóvel") {
         $("#dadosContrato").show();
         $(".campoLocacaoImovel").show();
     } 
-    else if($(that).val() === "Locação de Equipamento"){
+    else if($(that).val() === "Locação de Equipamento" || $(that).text() === "Locação de Equipamento"){
         $("#dadosContrato").show();
         $(".campoLocacaoEquipamento").show();
         $("#paginationEquipamentos").show();
         $("#paginationEquipamentos").removeClass("hidden");
-        anexosPorTipoDeContrato($(that).val());
+        if ($(that).val()) {
+            anexosPorTipoDeContrato($(that).val());
+            
+        }
     }
     else {
         $("#dadosContrato").hide();
