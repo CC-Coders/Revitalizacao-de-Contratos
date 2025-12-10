cde3ce5f5efc63262585661b3e4867d38252ac8c
Gabriel Persike <50594940+Gabriel-Persike@users.noreply.github.com>
2025-12-10 12:44:07 -0300

    Separa a lógica da paginação em um arquivo separado

--- Revitalizacao de Contratos/forms/Revitalização Contratos/js/Functions.js
+++ Revitalizacao de Contratos/forms/Revitalização Contratos/js/Functions.js
@@ -330,19 +330,6 @@ function inicializarPeriodoLocacao() {
 
 
 
-function setAtividadeAtivaProgresso(atividadesConcluidas) {
-    var counter = 0;
-    $(".wizard-progress")
-        .find("div")
-        .each(function () {
-            if (counter < atividadesConcluidas) {
-                $(this).addClass("completed");
-            } else if (counter == atividadesConcluidas) {
-                $(this).addClass("active");
-            }
-            counter++;
-        });
-}
 async function enviarSolicitacao() {
     const ATIVIDADE_ATUAL = $("#atividade").val();
 
@@ -370,37 +357,6 @@ async function enviarSolicitacao() {
     }
 }
 
-function mostrarPagina(indice, move) {
-    if (move == "set") {
-        var indiceAtivo = $(".pagination-active").attr("data-index");
-        if (indiceAtivo > indice) {
-            move = "prev";
-        } else {
-            move = "next";
-        }
-    }
-
-    $(".pagination-active").removeClass("pagination-active", 250);
-    $(`.pagination[data-index='${indice}']`).addClass("pagination-active", 250);
-
-    $(`.pagina.ativa[data-index!='${indice}']`).removeClass("ativa").addClass(move == "next" ? "escondida-para-esquerda" : "escondida-para-direita").css("position", "absolute");
-    $(`.pagina[data-index='${indice}']`).addClass("ativa", 250).removeClass("escondida-para-direita", 250).removeClass("escondida-para-esquerda", 250).css("position", "relative");
-    $(window).scrollTop(0)
-}
-function avancarPagina() {
-    var active = $(".pagination-active");
-    var index = $(active).nextAll(".pagination:not('.hidden'):first").attr("data-index");
-    if (index) {
-        mostrarPagina(index, "next");
-    }
-}
-function voltarPagina() {
-    var active = $(".pagination-active");
-    var index = $(active).prevAll(".pagination:not('.hidden')").attr("data-index");
-    if (index) {
-        mostrarPagina(index, "prev");
-    }
-}
 function handleFileUpload(inputId, descricaoArquivo) {
     const input = document.getElementById(inputId);
     const statusText = document.getElementById("textFileOrcamento");
